69146f2decdae97cc3c4d8078ec8859a37fc9a85
FeliOdras <ariane.feldmann@odras.de>
2019-06-12 09:28:22 +0200

    Adjusted footer and colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <p>Created by Ari @<a href="http://digitalcareerinsitute.org">DCI</a><br />
-                following basic <a href="https://reactjs.org/tutorial/tutorial.html" target="_blank">React Tutorial</a>
-            </p>
-            <div className="hoppink">#hotpink</div>
+  return (
+    <footer>
+      <p>
+        Created by Ari @<a href="http://digitalcareerinsitute.org">DCI</a>
+        <br />
+        following basic{" "}
+        <a href="https://reactjs.org/tutorial/tutorial.html" target="_blank">
+          React Tutorial
+        </a>
+      </p>
+      <div className="hotpink">#hotpink</div>
+    </footer>
+  );
+};
 
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
